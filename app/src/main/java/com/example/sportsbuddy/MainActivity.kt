991dd2cd04defd9ef9e8e8ee8bc01a0aa331bea4
신민석 <minsuk07672@naver.com>
2024-02-28 18:22:20 +0900

    Mainlayout

--- app/src/main/java/com/example/sportsbuddy/MainActivity.kt
+++ app/src/main/java/com/example/sportsbuddy/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.sportsbuddy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setSelectedItemId(R.id.home) // 두 번째 아이템의 ID를 전달하여 선택되도록 설정
     }
 }
